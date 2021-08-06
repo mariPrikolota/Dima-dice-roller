828daeeaecbdbbf432f0c4a0e85d6d8a34f81f78
VAmpersand <VP_ampersand_P@icloud.com>
2021-08-06 17:04:17 +0300

    Initial commit

--- app/src/main/java/com/example/dima/MainActivity.kt
+++ app/src/main/java/com/example/dima/MainActivity.kt
@@ -4,53 +4,50 @@ import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageView
+import androidx.core.view.isVisible
 import java.util.*
 
 
 class MainActivity: Activity() {
-    var dice:ImageView? = null
-    var counter:Int = 0
-    var timer: Timer? = null
-   // var its_run = false
-    var imageArray: IntArray= intArrayOf(
-        R.drawable.dice_1, R.drawable.dice_2, R.drawable.dice_3,
-        R.drawable.dice_4, R.drawable.dice_5, R.drawable.dice_6,
-    )
+    private var dice: ImageView? = null
+    private var counter: Int = 0
+    
 
+    val imageArray: IntArray = intArrayOf(
+        R.drawable.dice_1,
+        R.drawable.dice_2,
+        R.drawable.dice_3,
+        R.drawable.dice_4,
+        R.drawable.dice_5,
+        R.drawable.dice_6
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        dice=findViewById(R.id.imageView)
-
-
-
+        dice = findViewById(R.id.diceView)
     }
+
     fun onClikStart(view: View) {
-            start()
-
-    }
-
-    fun start(){
-
-        timer = Timer()
-        timer?.schedule(object: TimerTask(){
+        view.isVisible = false
+        val timer = Timer()
+        timer?.schedule(object : TimerTask() {
             override fun run() {
                 runOnUiThread {
-                dice?.setImageResource(imageArray[counter])
-                counter++
-                if (counter == 6) {
-                    val randomImage = imageArray.random()
-                    counter = 0
-                    dice?.setImageResource(randomImage)
-                    timer?.cancel()
-                   }
+                    dice?.setImageResource(imageArray.random())
+                    counter++
+
+                    if (counter == 15) {
+                        counter = 0
+                        timer?.cancel()
+                        view.isVisible = true
+                    }
                 }
             }
-        },0,500)
+        }, 0, 100)
 
-        }
     }
+}
 
 
 
